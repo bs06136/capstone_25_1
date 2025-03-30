ccb4d1777a1ce2aa5e27303be92e47c99ec4a4e9
ohjaiwoo <ojw7336@cau.ac.kr>
2025-03-30 13:01:44 +0900

    new 3 classes

--- OverCloud/obj/Debug/net6.0/OverCloud.AssemblyInfo.cs
+++ OverCloud/obj/Debug/net6.0/OverCloud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OverCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca9073bb5534f35d84db766e6c737572fcc069e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd2a6a74077cef3722cdca8297ac64925cbaf69")]
 [assembly: System.Reflection.AssemblyProductAttribute("OverCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OverCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
