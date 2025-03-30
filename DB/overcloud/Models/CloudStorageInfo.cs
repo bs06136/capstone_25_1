555f36c6404f8f61e08c72dd41034251a9206a01
LuisYou <48794664+LuisYou@users.noreply.github.com>
2025-03-30 15:39:36 +0900

    Add files via upload

---
+++ DB/overcloud/Models/CloudStorageInfo.cs
@@ -0,0 +1,10 @@
+namespace DB.overcloud.Models
+{
+    public class CloudStorageInfo
+    {
+        public int Id { get; set; }
+        public int AccountUserNum { get; set; }
+        public long TotalCapacity { get; set; }
+        public long UsedCapacity { get; set; }
+    }
+}
