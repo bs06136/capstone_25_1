04b61b3234d9882a9f9b744d4cd4f822fc5b1801
ohjaiwoo <ojw7336@cau.ac.kr>
2025-04-01 19:31:08 +0900

    new

---
+++ OverCloud/Services/FileUploadManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+//using System.Windows.MessageBox;
+
+
+namespace OverCloud.Services
+{
+    public class FileUploadManager
+    {
+        private readonly AccountService accountService;
+        private readonly GoogleDriveService googleDriveService;
+
+        public FileUploadManager(AccountService accountService, GoogleDriveService googleDriveService)
+        {
+            this.accountService = accountService;
+            this.googleDriveService = googleDriveService;
+        }
+
+        public async Task<bool> file_upload(string file_name)
+        {
+            var accounts = accountService.GetAllAccounts();
+            var googleAccount = accounts.FirstOrDefault(a => a.CloudType == "GoogleDrive");
+
+            if (googleAccount == null)
+            {
+              //  Sysyem.MessageBox.Show("Google Drive 계정이 없습니다.");
+                return false;
+            }
+
+            return await googleDriveService.UploadFileAsync(googleAccount.ID, file_name);
+        }
+    }
+}
