320d3fc3a21d3be3685be4cb3e567d43ad8ea10a
LuisYou <loisikr@naver.com>
2025-04-03 14:50:22 +0900

    모델 구조 변경

--- DB/overcloud/Models/CloudStorageInfo.cs
+++ DB/overcloud/Models/CloudStorageInfo.cs
@@ -3,14 +3,14 @@ namespace DB.overcloud.Models
     public class CloudStorageInfo
     {
         public int CloudStorageNum { get; set; }         // 클라우드 계정 고유 번호 (PK)
-        public int UserNum { get; set; }                 // 소유 사용자 (Account.user_num)
+        public int UserNum { get; set; }                 // 소유 사용자 (Account.user_num) (FK -> CloudAccountInfo)
 
         public string CloudType { get; set; }            // 클라우드 종류 (예: GoogleDrive)
         public string AccountId { get; set; }            // 클라우드 로그인 ID
         public string AccountPassword { get; set; }      // 클라우드 로그인 비밀번호 (필요시)
 
-        public int TotalSize { get; set; }             // 계정의 전체 용량
-        public int UsedSize { get; set; }              // 계정의 사용 중인 용량
+        public int TotalCapacity { get; set; }             // 계정 한 개의 전체 용량
+        public int UsedCapacity { get; set; }              // 계정 한 개의 사용 중인 용량
 
         public string accessToken { get; set; }         // Google Drive API 접근 토큰 
     }
