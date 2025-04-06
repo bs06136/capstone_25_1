98116582fc342c91d2f4c270bd7af5bafce8cfda
bs06136 <bszxcvbn@gmail.com>
2025-04-06 16:40:07 +0900

    temp fix

--- OverCloud/Services/FileUploadManager.cs
+++ OverCloud/Services/FileUploadManager.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using DB.overcloud.Models;
+using System.IO;
+using DB.overcloud.Repository;
+using overcloud;
 
 
 namespace OverCloud.Services
@@ -13,13 +17,17 @@ namespace OverCloud.Services
         private readonly AccountService accountService;
         private readonly GoogleDriveService googleDriveService;
         private readonly FileService fileService;
+        private readonly IFileRepository repo_file;
 
         public FileUploadManager()
         {
             accountService = new AccountService();
             googleDriveService = new GoogleDriveService();
-        }
 
+            repo_file = new FileRepository(DbConfig.ConnectionString);
+            fileService = new FileService(repo_file);
+
+        }
         
         public async Task<bool> file_upload(string file_name)
         {
@@ -35,9 +43,8 @@ namespace OverCloud.Services
                 return false;
             }
 
-<<<<<<< HEAD
             // 1. 업로드 수행
-            bool result = await googleDriveService.UploadFileAsync(googleAccount.AccountId, file_name);
+            bool result = await googleDriveService.UploadFileAsync(googleAccount.AccountId + "@gmail.com", file_name);
             if (!result) return false;
 
             // 2. 파일 정보 추출
@@ -56,11 +63,6 @@ namespace OverCloud.Services
 
             // 3. DB 저장
             return fileService.SaveFile(file);
-=======
-            var file = fileService.GetFile(fileId);
-            fileService.SaveFile(file);
-            return await googleDriveService.UploadFileAsync(googleAccount.AccountId + "@gmail.com", file_name);
->>>>>>> 7c94848e34286c149ea3e294c0dc94405b0d234d
         }
     }
 }
