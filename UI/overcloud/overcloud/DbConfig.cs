15412377d2e829c546e7159de890532de9ce2c6e
LuisYou <loisikr@naver.com>
2025-05-16 17:55:08 +0900

    DbConfig 변경

--- UI/overcloud/overcloud/DbConfig.cs
+++ UI/overcloud/overcloud/DbConfig.cs
@@ -3,6 +3,6 @@ namespace overcloud
     public static class DbConfig
     {
         public static string ConnectionString =>
-            "server=localhost;database=overcloud;uid=admin;pwd=admin;";
+            "server=overcloud.crywgqgyw87n.ap-northeast-2.rds.amazonaws.com;database=overcloud;uid=admin;pwd=admin123;";
     }
 }
