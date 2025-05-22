b829f455dad3a68bceae6ed64a3f495de745fda2
LuisYou <loisikr@naver.com>
2025-05-22 20:00:10 +0900

    Join_cooperation_...() 오류 수정

--- DB/overcloud/Repository/ICoopUserRepository.cs
+++ DB/overcloud/Repository/ICoopUserRepository.cs
@@ -6,7 +6,7 @@ namespace DB.overcloud.Repository
     {
         bool Add_cooperation_Cloud_Storage_pro_to_DB(string user_id_insert, string password, string user_id_mine);
         bool Delete_cooperation_Cloud_Storage_pro_to_DB(string user_id_insert, string user_id_mine);
-        bool Join_cooperation_Cloud_Storage_pro_to_DB(string user_id, string password);
+        bool Join_cooperation_Cloud_Storage_pro_to_DB(string user_id, string password, string user_id_mine);
         List<string> connected_cooperation_account_nums(string user_id);
     }
 }
