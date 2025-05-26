d0cb2c97942974a52c7fe1a695308a87069bab9e
LuisYou <loisikr@naver.com>
2025-05-26 18:44:28 +0900

    CloudStorageInfo 기본키 변경 및 인터페이스 수정

--- DB/overcloud/Repository/StorageRepository.cs
+++ DB/overcloud/Repository/StorageRepository.cs
@@ -13,17 +13,49 @@ namespace DB.overcloud.Repository
         {
             connectionString = connStr;
         }
-
-        public CloudStorageInfo GetCloud(int cloudStorageNum)
+        
+        // cloud_storage_num 을 생성하거나 기존 것을 반환만 함 (INSERT 는 외부에서 수행)
+        public int GetOrCreateCloudStorageNum(string cloudType, string accountId)
         {
-            Console.WriteLine(cloudStorageNum); 
             using var conn = new MySqlConnection(connectionString);
             conn.Open();
 
-            string query = "SELECT * FROM CloudStorageInfo WHERE cloud_storage_num = @cloudStorageNum";
+            // 동일한 클라우드가 있는지 확인
+            string checkQuery = @"
+                SELECT cloud_storage_num 
+                FROM CloudStorageInfo 
+                WHERE cloud_type = @type AND account_id = @account_id 
+                LIMIT 1";
+
+            using var checkCmd = new MySqlCommand(checkQuery, conn);
+            checkCmd.Parameters.AddWithValue("@type", cloudType);
+            checkCmd.Parameters.AddWithValue("@account_id", accountId);
+
+            var existingResult = checkCmd.ExecuteScalar();
+
+            if (existingResult != null)
+            {
+                return Convert.ToInt32(existingResult);
+            }
+            
+            else
+            {
+                string maxQuery = "SELECT COALESCE(MAX(cloud_storage_num), 0) FROM CloudStorageInfo";
+                using var maxCmd = new MySqlCommand(maxQuery, conn);
+                return Convert.ToInt32(maxCmd.ExecuteScalar()) + 1;
+            }
+        }
+
+        public CloudStorageInfo GetCloud(int cloudStorageNum, string userId)
+        {
+            using var conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            string query = "SELECT * FROM CloudStorageInfo WHERE cloud_storage_num = @num AND ID = @id";
 
             using var cmd = new MySqlCommand(query, conn);
-            cmd.Parameters.AddWithValue("@cloudStorageNum", cloudStorageNum);
+            cmd.Parameters.AddWithValue("@num", cloudStorageNum);
+            cmd.Parameters.AddWithValue("@id", userId);
 
             using var reader = cmd.ExecuteReader();
 
@@ -52,12 +84,16 @@ namespace DB.overcloud.Repository
             using var conn = new MySqlConnection(connectionString);
             conn.Open();
 
+            int cloudStorageNum = GetOrCreateCloudStorageNum(info.CloudType, info.AccountId);
+            info.CloudStorageNum = cloudStorageNum;
+
             string query = @"INSERT INTO CloudStorageInfo 
-                (ID, cloud_type, account_id, account_password, total_capacity, used_capacity, refresh_token, client_id, client_secret)
+                (cloud_storage_num, ID, cloud_type, account_id, account_password, total_capacity, used_capacity, refresh_token, client_id, client_secret)
                 VALUES 
-                (@id, @type, @accountId, @accountPw, @total, @used, @refresh, @clientId, @clientSecret)";
+                (@num, @id, @type, @accountId, @accountPw, @total, @used, @refresh, @clientId, @clientSecret)";
 
             using var cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@num", info.CloudStorageNum);
             cmd.Parameters.AddWithValue("@id", info.ID);
             cmd.Parameters.AddWithValue("@type", info.CloudType);
             cmd.Parameters.AddWithValue("@accountId", info.AccountId);
@@ -71,14 +107,16 @@ namespace DB.overcloud.Repository
             return cmd.ExecuteNonQuery() > 0;
         }
 
-        public bool DeleteCloudStorage(int cloudStorageNum)
+        public bool DeleteCloudStorage(int cloudStorageNum, string userId)
         {
             using var conn = new MySqlConnection(connectionString);
             conn.Open();
 
-            string query = "DELETE FROM CloudStorageInfo WHERE cloud_storage_num = @num";
+            string query = "DELETE FROM CloudStorageInfo WHERE cloud_storage_num = @num AND ID = @id";
+
             using var cmd = new MySqlCommand(query, conn);
             cmd.Parameters.AddWithValue("@num", cloudStorageNum);
+            cmd.Parameters.AddWithValue("@id", userId);
 
             return cmd.ExecuteNonQuery() > 0;
         }
@@ -91,28 +129,30 @@ namespace DB.overcloud.Repository
             string query = @"UPDATE CloudStorageInfo SET 
                                 total_capacity = @total,
                                 used_capacity = @used
-                            WHERE cloud_storage_num = @cloudNum";
+                            WHERE cloud_storage_num = @cloudNum AND ID = @id";
 
             using var cmd = new MySqlCommand(query, conn);
             cmd.Parameters.AddWithValue("@total", one_cloud.TotalCapacity);
             cmd.Parameters.AddWithValue("@used", one_cloud.UsedCapacity);
             cmd.Parameters.AddWithValue("@cloudNum", one_cloud.CloudStorageNum);
+            cmd.Parameters.AddWithValue("@id", one_cloud.ID);
 
             return cmd.ExecuteNonQuery() > 0;
         }
 
-        public bool UpdateRefreshToken(int cloudStorageNum, string refreshToken)
+        public bool UpdateRefreshToken(int cloudStorageNum, string userId, string refreshToken)
         {
             using var conn = new MySqlConnection(connectionString);
             conn.Open();
 
             string query = @"UPDATE CloudStorageInfo 
                             SET refresh_token = @token 
-                            WHERE cloud_storage_num = @id";
+                            WHERE cloud_storage_num = @id AND ID = @userId";
 
             using var cmd = new MySqlCommand(query, conn);
             cmd.Parameters.AddWithValue("@token", refreshToken);
             cmd.Parameters.AddWithValue("@id", cloudStorageNum);
+            cmd.Parameters.AddWithValue("@userId", userId);
 
             return cmd.ExecuteNonQuery() > 0;
         }
